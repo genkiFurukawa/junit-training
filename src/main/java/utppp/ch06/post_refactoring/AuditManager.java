d3f72ac907539f338f4058dc8935d121ab2ce56d
GENKI FURUKAWA <genki.furukawa@gmail.com>
2023-03-12 23:08:57 +0900

    [add] リスト6.9, 6.10, 6.11を写経

--- src/main/java/utppp/ch06/post_refactoring/AuditManager.java
+++ src/main/java/utppp/ch06/post_refactoring/AuditManager.java
@@ -1,24 +1,23 @@
-package utppp.ch06.pre_refactoring;
+package utppp.ch06.post_refactoring;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
- * リスト6.8をJavaに移植したもの
+ * リスト6.8のコードだとファイルシステムにアクセスしていてテストすることが難しくなっている。
+ * 単体テストの条件のうち、「実行時間が短いこと」と「隔離された状態で実行されること」を満たしていない
  */
 public class AuditManager {
     private final int maxEntriesPerFile;
     private final String directoryName;
+    private final IFileSystem fileSystem;
 
-    public AuditManager(int maxEntriesPerFile, String directoryName) {
+    public AuditManager(int maxEntriesPerFile, String directoryName, IFileSystem fileSystem) {
         this.maxEntriesPerFile = maxEntriesPerFile;
         this.directoryName = directoryName;
+        this.fileSystem = fileSystem;
     }
 
     /**
@@ -27,8 +26,8 @@ public class AuditManager {
      * @param visitorName 訪問者の名前
      * @param timeOfVisit 訪問時刻
      */
-    public void addRecord(String visitorName, Date timeOfVisit) throws IOException {
-        var filePaths = Files.list(Paths.get(directoryName)).toList();
+    public void addRecord(String visitorName, LocalDateTime timeOfVisit) {
+        var filePaths = fileSystem.getFiles(directoryName);
         var sorted = filePaths.stream().sorted().toList();
 
         String newRecord = visitorName + ";" + timeOfVisit;
@@ -36,27 +35,21 @@ public class AuditManager {
         // NOTE: ファイルがない時はaudit_01.txtファイルに訪問者と日時のレコードを追加する
         if (filePaths.size() == 0) {
             Path path = Paths.get(directoryName, "audit_01.txt");
-            FileWriter filewriter = new FileWriter(path.toFile());
-            filewriter.write(newRecord + "\r\n");
-            filewriter.close();
+            fileSystem.writeAllText(path, newRecord);
             return;
         }
 
         // NOTE: 最後のファイルを取得して中身をチェックし、書き込み上限行数を超えていないか確認する。
         // NOTE: 書き込み上限行数を超えている場合は新しいファイルを作成し、新しいファイルにレコードを追加する
         Path path = sorted.get(sorted.size() - 1);
-        List<String> lines = Files.readAllLines(path);
+        List<String> lines = fileSystem.readAllLines(path);
 
         if (lines.size() < maxEntriesPerFile) {
-            FileWriter filewriter = new FileWriter(path.toFile(), true);
-            filewriter.write(newRecord + "\r\n");
-            filewriter.close();
+            fileSystem.writeAllText(path, newRecord);
         } else {
             String newFileName = "audit_" + String.format("%02d", filePaths.size() + 1) + ".txt";
             Path newFilepath = Paths.get(directoryName, newFileName);
-            FileWriter filewriter = new FileWriter(newFilepath.toFile());
-            filewriter.write(newRecord + "\r\n");
-            filewriter.close();
+            fileSystem.writeAllText(path, newRecord);
         }
     }
 }
---
+++ src/main/java/utppp/ch06/post_refactoring/IFileSystem.java
@@ -0,0 +1,30 @@
+package utppp.ch06.post_refactoring;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public interface IFileSystem {
+    /**
+     * ファイルの一覧を取得する
+     *
+     * @param directoryName 　directoryName
+     * @return ファイルの一覧
+     */
+    List<Path> getFiles(String directoryName);
+
+    /**
+     * ファイルに内容を書き込む
+     *
+     * @param filePath filePath
+     * @param content  content
+     */
+    void writeAllText(Path filePath, String content);
+
+    /**
+     * ファイルの全行を取得する
+     *
+     * @param filePath filePath
+     * @return 全行
+     */
+    List<String> readAllLines(Path filePath);
+}
